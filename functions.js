4873bc68294b70639d05e6db1c9f18fabc215e7a
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-22 10:49:12 -0600

    Add answers

--- functions.js
+++ functions.js
@@ -1,13 +1,39 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+var printGreeting = () => {
+  console.log('Welcome aboard! CHOO CHOO!')
+}
+printGreeting()
+printGreeting()
+printGreeting()
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function printPersonalGreeting (firstName) {
+  console.log(`Hello ${firstName}! We're so glad to have you!`)
+}
+printPersonalGreeting('Ian')
+printPersonalGreeting('Allie')
+printPersonalGreeting('Max')
 
 
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-
+// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company,
+// and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print
+// out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+var companyPayRange = function (companyName, minPay, maxPay) {
+  var payRange = maxPay - minPay
+  console.log(`${companyName} has a pay range of ${payRange}.`)
+}
+companyPayRange('Capitalism', 0, 10000000000)
+companyPayRange('Apple', 45000, 250000)
 
 // 4: Write a function that satifies the following interaction pattern:
+
+function checkStock (inventory, product) {
+  if (inventory >= 4) {
+    console.log(`${product} is stocked`)
+  } else if (inventory <= 0 ) {
+    console.log(`${product} - OUT of stock!`)
+  } else console.log(`${product} - running low!`)
+}
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
