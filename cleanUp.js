7eb603773c353e28720bb0b096bd3ef4ee13c464
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-22 10:31:04 -0600

    Cleaned syntax

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,52 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
+// added ;s
+// probably should use prompt() instead of console.log() though
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
+// cleaned up syntax for legibility
+
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
 
+//spelled out function, cleaned up syntac
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+function averageTwoNums(num1, num2)  {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  console.log(`The average is: ${avg}.`)
+}
 
-  console.log(`the average is: ${avg}`.)
-  }
+averageTwoNums(10, 20)
+
+// changed name to be more specific. cleaned up syntax. Called function
